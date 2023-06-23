28f910efb17836b6c97ad670a6b64caf5617c69f
Jazmine Galdos <jazminegaldos@gmail.com>
2023-06-23 12:02:10 -0500

    Update Script_Problem_set_1.R

--- scripts/Script_Problem_set_1.R
+++ scripts/Script_Problem_set_1.R
@@ -4,7 +4,6 @@
 # correo: ds.peralta@uniandes.edu.co
 # fecha: 20/06/2023
 
-
 # Preparación -------------------------------------------------------------
 
 rm(list = ls()) # Limpiar Rstudio
@@ -24,79 +23,95 @@ getwd()
 # Extracting Data ---------------------------------------------------------
 my_url<-"https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html"
 html_code<-read_html(my_url)
-table1 <- html_code %>% html_nodes("table") %>% .[[1]]
-table1<-table1 %>% html_table()
-head(table1)
+GEIH <- html_code %>% html_nodes("table") %>% .[[1]]
+GEIH<-GEIH %>% html_table()
+head(GEIH)
 
 
 # Organizing data ---------------------------------------------------------
 
-table1<- subset(table1, select=c(directorio, secuencia_p, ingtot, ingtotes, ocu, pet, mes, age, p6210, p6210s1, depto, clase, oficio, fex_c, fex_dpto, totalHoursWorked, sex, p6050, maxEducLevel, p6426, formal, p6500, cuentaPropia, y_ingLab_m, y_ingLab_m_ha, y_salary_m, y_salary_m_hu, y_total_m, y_total_m_ha))
+GEIH<- subset(GEIH, select=c(directorio, secuencia_p, ingtot, ingtotes, ocu, pet, mes, age, p6210, p6210s1, depto, clase, oficio, fex_c, fex_dpto, totalHoursWorked, sex, p6050, maxEducLevel, p6426, formal, p6500, cuentaPropia, y_ingLab_m, y_ingLab_m_ha, y_salary_m, y_salary_m_hu, y_total_m, y_total_m_ha))
 
-colnames(table1)[colnames(table1) == ""] ="index"
-colnames(table1)[colnames(table1) == "p6210"] = "educ"
-colnames(table1)[colnames(table1) == "p6210s1"] = "grad_educ"
-colnames(table1)[colnames(table1) == "clase"] = "urbano"
-colnames(table1)[colnames(table1) == "p6050"] = "parentesco_jh"
-colnames(table1)[colnames(table1) == "p6426"] = "tiempo_trab"
-colnames(table1)[colnames(table1) == "p6500"] = "sueldo_mens"
+colnames(GEIH)[colnames(GEIH) == ""] ="index"
+colnames(GEIH)[colnames(GEIH) == "p6210"] = "educ"
+colnames(GEIH)[colnames(GEIH) == "p6210s1"] = "grad_educ"
+colnames(GEIH)[colnames(GEIH) == "clase"] = "urbano"
+colnames(GEIH)[colnames(GEIH) == "p6050"] = "parentesco_jh"
+colnames(GEIH)[colnames(GEIH) == "p6426"] = "tiempo_trab"
+colnames(GEIH)[colnames(GEIH) == "p6500"] = "sueldo_mens" #es sueldo por hora pero se usa esa variable solo para temas de código
 
-table1$llave<- paste(table1$directorio, table1$secuencia_p)
-table1<-as.data.frame(table1)
+GEIH$llave<- paste(GEIH$directorio, GEIH$secuencia_p)
+GEIH<-as.data.frame(GEIH)
 
 # Cleaning/Filtering data -------------------------------------------------
 
-suel_prom_hog<-aggregate(sueldo_mens~directorio+secuencia_p, data=table1 ,FUN=(function (x){ifelse(sum(x==0)>0 & sum(x !=0) >0, mean(x[x>0]), mean(x))}))
+suel_prom_hog<-aggregate(sueldo_mens~directorio+secuencia_p, data=GEIH ,FUN=(function (x){ifelse(sum(x==0)>0 & sum(x !=0) >0, mean(x[x>0]), mean(x))}))
 suel_prom_hog$llave<- paste(suel_prom_hog$directorio, suel_prom_hog$secuencia_p)
 
-if (is.na(table1$sueldo_mens)) {
-  table1$sueldo_mens[match(suel_prom_hog$llave, table1$llave)]<- suel_prom_hog$sueldo_mens
+if (is.na(GEIH$sueldo_mens)) {
+  GEIH$sueldo_mens[match(suel_prom_hog$llave, GEIH$llave)]<- suel_prom_hog$sueldo_mens
 }
 
-table1<- table1[table1$sueldo_mens !=0,] #sacamos los 0
+GEIH<- GEIH[GEIH$sueldo_mens !=0,] #sacamos los 0
 
-names(table1)
-#missing<-is.na(table1$p6500)
-#table1<-subset(table1, subset=!missing) #sacamos los NA
+names(GEIH)
+#missing<-is.na(GEIH$p6500)
+#GEIH<-subset(GEIH, subset=!missing) #sacamos los NA
 
 
+# Import filtered data ----------------------------------------------------
+
+load("../stores/GEIH.RData")
+summary(GEIH)
 # Question 3- Estimating the Age-wage profile profile--------
 
 
 #Model: log(w) = β1 + β2Age + β3Age2 + u
 
 #Variable Salario:
-table1<-table1%>% mutate(logw=log(sueldo_mens))
-table1<-table1%>% mutate(age2=age^2)
-summary(table1$logw)
+#GEIH<-GEIH%>% mutate(logw=log(sueldo_mens))
+#GEIH<-GEIH%>% mutate(age2=age^2)
+#summary(GEIH$logw)
+GEIH<-GEIH%>%mutate(edad2=edad^2)
 
-reg_w_age<-lm(formula=logw~age+age2, data=table1, subset=sueldo_mens!=0,weights=fex_c)
-reg_w_age2<-lm(formula=logw~age+age2+ educ +sex +urbano+formal,data=table1, subset=sueldo_mens!=0,weights=fex_c)
+reg_w_age<-lm(formula=log_salario_hora2~edad+edad2, data=GEIH, weights=fex_c)
+#reg_w_age2<-lm(formula=logw~age+age2+ educ +sex +urbano+formal,data=GEIH, subset=sueldo_mens!=0,weights=fex_c)
+summary(reg_w_age)
 
-stargazer(reg_w_age, reg_w_age2,type="text", keep=c("age","age2") ,title="Tabla X: Resultado de la Regresión Salario-Edad", 
-          dep.var.labels="Log(salario)",covariate.labels=c("Edad","Edad2"),omit.stat=c("ser","f","adj.rsq"), out="../views/age_wage1.html", add.lines=list(c('Variables de Control', 'No','Si')),notes="Las variables de control incluyen educación, género, urbano rural e informal", notes.align = "l")
+coefs_w_age<-reg_w_age$coef
+b0_w_age<-coefs_w_age[1]
+b1_w_age<-coefs_w_age[2]
+b2_w_age<-coefs_w_age[3]
+
+age_mean<-mean(age)
+age_mea2<-mean(age2)
+
+stargazer(reg_w_age,type="text",title="Tabla X: Resultado de la Regresión Salario-Edad", keep=c("edad","edad2"), 
+          dep.var.labels="Log(salario)",covariate.labels=c("Edad","Edad2"),omit.stat=c("ser","f","adj.rsq"), out="../views/age_wage1.html", add.lines=list(c('Variables de Control', 'No')))
+
+
 
 #Prediction
-table1$yhat1<-ifelse(table1$sueldo_mens!=0, predict(reg_w_age), 0)
-table1$yhat2<-ifelse(table1$sueldo_mens!=0, predict(reg_w_age2), 0)
+GEIH$yhat1<-ifelse(GEIH$log_salario_hora2!=0, predict(reg_w_age), 0)
+#GEIH$yhat2<-ifelse(GEIH$log_salario_hora2!=0, predict(reg_w_age2), 0)
 
 
-summ_table1 <- table1 %>%  
+summ_GEIH <- GEIH %>%  
   group_by(
-    age, age2
+    edad, edad2
   ) %>%  
   summarize(
-    mean_y = mean(logw),
+    mean_y = mean(log_salario_hora2),
     yhat_reg = mean(yhat1), .groups="drop"
   ) 
 
-ggplot(summ_table1) + 
+ggplot(summ_GEIH) + 
   geom_point(
-    aes(x = age, y = mean_y),
+    aes(x = edad, y = mean_y),
     color = "5", size = 2
   ) + 
   geom_line(
-    aes(x = age, y = yhat_reg), 
+    aes(x = edad, y = yhat_reg), 
     color = "8", size = 1.5
   ) + 
   labs(
@@ -105,6 +120,9 @@ ggplot(summ_table1) +
     y = "Log Sueldos"
   ) +
   theme_bw()
+
+
+
 
 # Question 4: The gender earnings GAP -------------------------------------
 
