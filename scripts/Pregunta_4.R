32b25dab3041b6b114809b3377262df5985776a6
Jazmine Galdos <jazminegaldos@gmail.com>
2023-06-24 12:49:06 -0500

    Update Pregunta_4.R

--- scripts/Pregunta_4.R
+++ scripts/Pregunta_4.R
@@ -27,17 +27,50 @@ GEIH<-GEIH[!is.na(GEIH$log_salario_hora),] #para poder correr todo el código
 #Model1: log(w) = β1 + β2Female + u
 
 reg_w_fem<-lm(formula=log_salario_hora~mujer, data=GEIH) #modelo general
-reg_w_fem2<-lm(formula=log_salario_hora~mujer, subset=mujer==1, data=GEIH) #modelo con controles
-reg_w_fem3<-lm(formula=log_salario_hora~edad+edad2, subset=mujer==0, data=GEIH) #modelo con mas controles
+reg_w_fem$AIC<-AIC(reg_w_fem) #Akaike para modelo general
 
-reg_w_age$AIC<-AIC(reg_w_age) #Akaike para modelo general
-reg_w_age_mujer$AIC<-AIC(reg_w_age_mujer) #Akaike para modelo mujeres
-reg_w_age_hombre$AIC<-AIC(reg_w_age_hombre) #Akaike para modelo hombres
 
-#Con los tres modelos
-stargazer(reg_w_age, reg_w_age_mujer, reg_w_age_hombre, type="text",title="Tabla 3.1: Regresión Salario-Edad", keep=c("edad","edad2"), 
-          dep.var.labels="Log(salario)",covariate.labels=c("Edad","Edad2"),omit.stat=c("ser","f","adj.rsq","aic"), out="../views/age_wage2.html",
-          add.lines=list(c("AIC", round(AIC(reg_w_age),1), round(AIC(reg_w_age_mujer),1), round(AIC(reg_w_age_hombre),1))))
+#Modelo2: log(w) = β1 + β2Female + ocupacion + eduacion+ edad+ edad2 + u [usando FWL]
+
+GEIH<-GEIH %>% mutate(muj_res=lm(mujer~edad+edad2+educacion_tiempo+as.factor(ocupacion),GEIH)$residuals,#capturamos los residuales de mujer
+                      sal_res=lm(log_salario_hora~edad+edad2+educacion_tiempo+as.factor(ocupacion),GEIH)$residuals) #capturamos los residuales del salario
+
+reg_fwl1<-lm(sal_res~muj_res,GEIH) #el coeficiente de mujer debería salir igual que si lo corremos como lm(log_salario_hora~mujer+edad+edad2+educacion_tiempo+as.factor(ocupacion))
+
+#Con los dos modelos
+stargazer(reg_w_fem, reg_fwl1, type="text",title="Tabla 4.1: Regresión Salario-Female", 
+          dep.var.labels=c("Ln(salario)","Ln(salario)"),covariate.labels=c("Mujer","Mujer FWL"),omit.stat=c("ser","f","adj.rsq","aic"), out="../views/age_fem.html",
+          add.lines=list(c("AIC", round(AIC(reg_w_fem),1), round(AIC(reg_w_fem2),1),'Variables de Control', 'No','Si')), notes="El modelo FWL (2) ha sido calculado con las variable de control edad, educación, y ocupación.", notes.align="l")
+
+
+####AGREGAR una linea mas para poner variables de control
+
+
+
+#Utilizamos bootstrap para calcular los coeficientes y errores estándares
+
+#Función para Bootstrap
+model_fwl_boot<- function(data, index) {
+  f<- lm(formula=sal_res~muj_res, data, subset=index)
+  
+  coefs<-f$coefficients
+  b2<-coefs[2]
+  
+  return(b2)
+}
+
+model_fwl_boot(GEIH,1:nrow(GEIH)) #para verificar que nos de el mismo coeficiente de mujer
+
+err_est_fwl_boot<-boot(GEIH,model_fwl_boot,R=1000)
+err_est_fwl_boot
+se_fwl<- apply(err_est_fwl_boot$t,2,sd)[1] #grabamos el valor del error estándar en el objeto se
+
+
+#Plot
+
+
+
+
 
 
 
