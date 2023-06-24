6497a10426ad2d83699cd80639cda587d3c9f3ef
Jazmine Galdos <jazminegaldos@gmail.com>
2023-06-23 22:04:42 -0500

    Update Script_Problem_set_1.R

--- scripts/Script_Problem_set_1.R
+++ scripts/Script_Problem_set_1.R
@@ -8,9 +8,9 @@
 
 rm(list = ls()) # Limpiar Rstudio
 
-options(scipen = 20,  digits=3)
+options(scipen = 20,  digits=5)
 require(pacman)
-p_load(ggplot2, rio, tidyverse, skimr, caret, rvest, magrittr, rstudioapi, stargazer, boot) # Cargar varios paquetes al tiempo
+p_load(ggplot2, rio, tidyverse, skimr, caret, rvest, magrittr, rstudioapi, stargazer, boot, openxlsx) # Cargar varios paquetes al tiempo
 
 
 #Definir el directorio
@@ -19,65 +19,26 @@ path_folder<-dirname(path_script)
 setwd(path_folder)
 getwd()
 
-
-# Extracting Data ---------------------------------------------------------
-my_url<-"https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html"
-html_code<-read_html(my_url)
-GEIH <- html_code %>% html_nodes("table") %>% .[[1]]
-GEIH<-GEIH %>% html_table()
-head(GEIH)
-
-
-# Organizing data ---------------------------------------------------------
-
-GEIH<- subset(GEIH, select=c(directorio, secuencia_p, ingtot, ingtotes, ocu, pet, mes, age, p6210, p6210s1, depto, clase, oficio, fex_c, fex_dpto, totalHoursWorked, sex, p6050, maxEducLevel, p6426, formal, p6500, cuentaPropia, y_ingLab_m, y_ingLab_m_ha, y_salary_m, y_salary_m_hu, y_total_m, y_total_m_ha))
-
-colnames(GEIH)[colnames(GEIH) == ""] ="index"
-colnames(GEIH)[colnames(GEIH) == "p6210"] = "educ"
-colnames(GEIH)[colnames(GEIH) == "p6210s1"] = "grad_educ"
-colnames(GEIH)[colnames(GEIH) == "clase"] = "urbano"
-colnames(GEIH)[colnames(GEIH) == "p6050"] = "parentesco_jh"
-colnames(GEIH)[colnames(GEIH) == "p6426"] = "tiempo_trab"
-colnames(GEIH)[colnames(GEIH) == "p6500"] = "sueldo_mens" #es sueldo por hora pero se usa esa variable solo para temas de código
-
-GEIH$llave<- paste(GEIH$directorio, GEIH$secuencia_p)
-GEIH<-as.data.frame(GEIH)
-
-# Cleaning/Filtering data -------------------------------------------------
-
-suel_prom_hog<-aggregate(sueldo_mens~directorio+secuencia_p, data=GEIH ,FUN=(function (x){ifelse(sum(x==0)>0 & sum(x !=0) >0, mean(x[x>0]), mean(x))}))
-suel_prom_hog$llave<- paste(suel_prom_hog$directorio, suel_prom_hog$secuencia_p)
-
-if (is.na(GEIH$sueldo_mens)) {
-  GEIH$sueldo_mens[match(suel_prom_hog$llave, GEIH$llave)]<- suel_prom_hog$sueldo_mens
-}
-
-GEIH<- GEIH[GEIH$sueldo_mens !=0,] #sacamos los 0
-
-names(GEIH)
-#missing<-is.na(GEIH$p6500)
-#GEIH<-subset(GEIH, subset=!missing) #sacamos los NA
-
-
 # Import filtered data ----------------------------------------------------
 
-load("../stores/GEIH.RData")
+GEIH <- read_excel("../stores/GEIH")
 summary(GEIH)
+names(GEIH)
+
 # Question 3- Estimating the Age-wage profile profile--------
 
 
 #Model: log(w) = β1 + β2Age + β3Age2 + u
 
-#Variable Salario:
-#GEIH<-GEIH%>% mutate(logw=log(sueldo_mens))
-#GEIH<-GEIH%>% mutate(age2=age^2)
-#summary(GEIH$logw)
-GEIH<-GEIH%>%mutate(edad2=edad^2)
+reg_w_age<-lm(formula=log_salario_hora~edad+edad2, data=PREGUNTA3)
+reg_w_age_mujer<-lm(formula=log_salario_hora~edad+edad2, subset=sexo==0, data=PREGUNTA3)
+reg_w_age_hombre<-lm(formula=log_salario_hora~edad+edad2, subset=sexo==1, data=PREGUNTA3)
 
-reg_w_age<-lm(formula=log_salario_hora2~edad+edad2, data=GEIH, weights=fex_c)
-#reg_w_age2<-lm(formula=logw~age+age2+ educ +sex +urbano+formal,data=GEIH, subset=sueldo_mens!=0,weights=fex_c)
+#reg_w_age<-lm(formula=log_salario_hora~edad+edad2, data=PREGUNTA3, weights=fex_c)
+#reg_w_age2<-lm(formula=logw~age+age2+ educ +sex +urbano+formal,data=PREGUNTA3, subset=sueldo_mens!=0,weights=fex_c)
 summary(reg_w_age)
 
+##AGREGAR MUJER Y HOMBRE EN TABLA- AIC BIC
 stargazer(reg_w_age,type="text",title="Tabla X: Resultado de la Regresión Salario-Edad", keep=c("edad","edad2"), 
           dep.var.labels="Log(salario)",covariate.labels=c("Edad","Edad2"),omit.stat=c("ser","f","adj.rsq"), out="../views/age_wage1.html", add.lines=list(c('Variables de Control', 'No')))
 
@@ -86,25 +47,25 @@ b1_w_age<-coefs_w_age[1]
 b2_w_age<-coefs_w_age[2]
 b3_w_age<-coefs_w_age[3]
 
-edad_mean<-mean(GEIH$edad)
-edad_mea2<-mean(GEIH$edad2)
+edad_mean<-mean(PREGUNTA3$edad)
+edad_mea2<-mean(PREGUNTA3$edad2)
 
 #Predict yhat
-GEIH$yhat<-ifelse(GEIH$log_salario_hora2!=0, predict(reg_w_age), 0)
-#GEIH$yhat<-predict(reg_w_age2)
+PREGUNTA3$yhat<-ifelse(PREGUNTA3$log_salario_hora2!=0, predict(reg_w_age), 0)
+#PREGUNTA3$yhat<-predict(reg_w_age2)
 
 
-#Model in sample fit
-summ_GEIH <- GEIH %>%  
+#Model in sample fit #EXTRA
+summ_PREGUNTA3 <- PREGUNTA3 %>%  
   group_by(
     edad, edad2
   ) %>%  
   summarize(
-    mean_y = mean(log_salario_hora2),
+    mean_y = mean(log_salario_hora),
     yhat_reg = mean(yhat), .groups="drop"
   ) 
 
-ggplot(summ_GEIH) + 
+ggplot(summ_PREGUNTA3) + 
   geom_point(
     aes(x = edad, y = mean_y),
     color = "5", size = 2
@@ -120,8 +81,6 @@ ggplot(summ_GEIH) +
   ) +
   theme_bw()
 
-#ponemos medidas de ajuste? como aic bic c?
-
 #Model: log(w) = β1 + β2Age + β3Age2 + u
 #Prediction
 
@@ -131,7 +90,7 @@ edad_max
 #standard errors
 
 model_wage_age_fn<- function(data, index, edad_barra=mean(edad)) {
-                    f<- lm(formula=log_salario_hora2~edad+edad2, data, subset=index)
+                    f<- lm(formula=log_salario_hora~edad+edad2, data, subset=index)
                     
                     coefs<-f$coefficients
                     b2<-coefs[2]
@@ -141,10 +100,15 @@ model_wage_age_fn<- function(data, index, edad_barra=mean(edad)) {
                     return(edad_max_bt)
 }
 
-model_wage_age_fn(GEIH,1:nrow(GEIH))
+model_wage_age_fn(PREGUNTA3,1:nrow(PREGUNTA3))
 
-err_est_wage_age<-boot(GEIH,model_wage_age_fn,R=1000)
+err_est_wage_age<-boot(PREGUNTA3,model_wage_age_fn,R=1000)
 err_est_wage_age
+
+g3 <- ggplot(db, aes(x=edad, y=predic2a)) + 
+  geom_point(col = "red" , size = 1.5) +
+  geom_smooth(method='lm', formula=y~x, se=FALSE, col='brown1') +
+  theme_light()
 
 
 # Question 4: The gender earnings GAP -------------------------------------
