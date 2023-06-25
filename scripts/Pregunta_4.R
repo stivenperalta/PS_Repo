a019e9afa3d07019005ac3da4e2d3d17e0145055
Jazmine Galdos <jazminegaldos@gmail.com>
2023-06-25 13:06:21 -0500

    Update Pregunta_4.R

--- scripts/Pregunta_4.R
+++ scripts/Pregunta_4.R
@@ -18,7 +18,7 @@ getwd()
 GEIH <- read_excel("../stores/GEIH") #ajustar ruta store
 summary(GEIH)
 GEIH$relacion_laboral[is.na(GEIH$relacion_laboral)]<-9 #reemplazamos los missings por 9
-GEIH$tamaño_empresa[is.na(GEIH$tamaño_empresa)]<-1
+GEIH<-GEIH[complete.cases(GEIH[,c("tamaño_empresa")]),] #imputamos las personas sin información del tamaño de la empresa
 names(GEIH)
 
 # Question 4: The gender earnings GAP -------------------------------------
@@ -28,24 +28,28 @@ names(GEIH)
 reg_w_fem<-lm(formula=log_salario_hora_imputado~mujer, data=GEIH) #modelo general
 reg_w_fem$AIC<-AIC(reg_w_fem) #Akaike para modelo general
 
-
 #Modelo2: log(w) = β1 + β2Female + relacion_laboral + eduacion+ edad+ edad2 + tamaño empresa + u [usando FWL]
 
-GEIH<-GEIH %>% mutate(muj_res=lm(mujer~edad+edad2+educacion_tiempo+as.factor(relacion_laboral)+tamaño_empresa,GEIH)$residuals,#capturamos los residuales de mujer
+GEIH<-GEIH %>% mutate(muj_res=lm(mujer~edad+edad2+educacion_tiempo+as.factor(relacion_laboral)+as.factor(tamaño_empresa),GEIH)$residuals,#capturamos los residuales de mujer
                       sal_res=lm(log_salario_hora_imputado~edad+edad2+educacion_tiempo+as.factor(relacion_laboral)+tamaño_empresa,GEIH)$residuals) #capturamos los residuales del salario
 
-reg_fwl1<-lm(sal_res~muj_res,GEIH) #el coeficiente de mujer debería salir igual que si lo corremos como lm(log_salario_hora_imputado~mujer+edad+edad2+educacion_tiempo+as.factor(relacion_laboral))
+reg_fwl1<-lm(sal_res~muj_res,GEIH) #el coeficiente de mujer debería salir igual que si lo corremos como lm(log_salario_hora_imputado~mujer+edad+edad2+educacion_tiempo+as.factor(relacion_laboral)+as.factor(tamaño_empresa))
+
+#probamos para ver si sale el mismo coeficiente
+lm(log_salario_hora_imputado~mujer+edad+edad2+educacion_tiempo+as.factor(relacion_laboral)+as.factor(tamaño_empresa),GEIH)
+reg_fwl1
+
 
 #Con los dos modelos
 stargazer(reg_w_fem, reg_fwl1, type="text",title="Tabla 4.1: Regresión Salario-Mujer", 
-          dep.var.labels=c("Ln(salario)","Ln(salario)"),covariate.labels=c("Mujer","Mujer FWL"),omit.stat=c("ser","f","adj.rsq","aic"), out="../views/age_fem.html",
+          dep.var.labels=c("Ln(salario)","Ln(salario)"),covariate.labels=c("Mujer","Mujer FWL"), omit.stat=c("ser","f","adj.rsq","aic"), 
+          out="../views/age_fem.html", keep= c("mujer", "muj_res"),
           add.lines=list(c("AIC", round(AIC(reg_w_fem),1), round(AIC(reg_fwl1),1)),c('Variables de Control', 'No','Si')), 
           notes=c("El modelo FWL (2) ha sido calculado con las variable de control", 
           "edad, educación, ocupación y tamaño de la empresa."), notes.align="c")
 
 #Utilizamos bootstrap para calcular los coeficientes y errores estándares
 
-set.seed(12345)
 #Función para Bootstrap
 model_fwl_boot<- function(data, index) {
   f<- lm(formula=sal_res~muj_res, data, subset=index)
@@ -62,21 +66,28 @@ model_fwl_boot(GEIH,1:nrow(GEIH)) #para verificar que nos de el mismo coeficient
 err_est_fwl_boot<-boot(GEIH,model_fwl_boot,R=1000)
 err_est_fwl_boot
 
-se_fwl<- apply(err_est_fwl_boot$t,2,sd)[1] #grabamos el valor del error estándar en el objeto se
+# Extraemos el coeficiente b2 y el error estándard
+t1_stat <- err_est_fwl_boot$t0
+se_t1 <- sd(err_est_fwl_boot$t)
+fwl_boot<-c(t1_stat,se_t1)
 
-# tabla manual con resultados de bootstrap
-bootstrap_table <- data.frame(
-  estimador = -0.01926,
-  bias = -0.00007,
-  std.error = 0.01159)
+#Extraemos el coeficientes de antiguas regresiones
+t1_reg<-reg_w_fem$coef[2]
+se_reg<-coef(summary(reg_w_fem))[, "Std. Error"][2]
+reg_muj<-c(t1_reg,se_reg)
 
-bootstrap_table
+t1_fwl<-reg_fwl1$coef[2]
+se_fwl<-coef(summary(reg_fwl1))[, "Std. Error"][2]
+reg_fwl<-c(t1_fwl,se_fwl)  
+
+tags<-c("Mujer","Std. Error")
+
+#Juntamos todos los resultados de las regresiones
+comparison.df<-format(data.frame(Modelo=tags,Regression=reg_muj,FWL=reg_fwl,FWL_BOOT=fwl_boot),digits=3)
 
 # Exportar la tabla
 ruta <- "../views/fwl_table.html" 
-fwl_boot <- kable(bootstrap_table, format = "html", align = "c", caption = "Estimacion Bootstrap FWL") %>%
-  kable_styling() %>%
-  row_spec(1, bold = FALSE)
+fwl_boot <- kable(comparison.df, format = "html", align = "c", caption = "Comparación de Modelos") %>%
 cat(fwl_boot, file = ruta)
 
 
